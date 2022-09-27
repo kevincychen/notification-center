3256dc1785ea202ecb66b49bd91b40f5e586c42a
1302074-NB\Wistronits <KevinCYChen@wistronits.com>
2022-09-27 14:38:07 +0800

    Add callback function to get topic related messages.

--- notification-center/src/main/java/com/cdfholding/notificationcenter/controller/AdminController.java
+++ notification-center/src/main/java/com/cdfholding/notificationcenter/controller/AdminController.java
@@ -1,13 +1,14 @@
 package com.cdfholding.notificationcenter.controller;
 
-import com.cdfholding.notificationcenter.dto.AllowedUserApplyRequest;
-import com.cdfholding.notificationcenter.dto.AllowedUserApplyResponse;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.cdfholding.notificationcenter.dto.AllowedUserApplyRequest;
+import com.cdfholding.notificationcenter.dto.AllowedUserApplyResponse;
 
 @RestController
 public class AdminController {
@@ -21,11 +22,24 @@ public class AdminController {
     @PostMapping(path = "/apply")
     public AllowedUserApplyResponse apply(@RequestBody AllowedUserApplyRequest request) {
         request.setType("apply");
-        ListenableFuture<SendResult<String, AllowedUserApplyRequest>> resultTemplate = 
+        ListenableFuture<SendResult<String, AllowedUserApplyRequest>> resultFuture = 
             kafkaTemplate.send("allowed-user-command", request.getAdUser(), request);
-        while(null != resultTemplate.get()) {
+        // 0927 add callback
+        resultFuture.addCallback(new ListenableFutureCallback<SendResult<String, AllowedUserApplyRequest>>() {
+
+          @Override
+          public void onFailure(Throwable ex) {
+            System.out.println("Unable to send message=[allowed-user-command] due to : " + ex.getMessage());           
+          }
+
+          @Override
+          public void onSuccess(SendResult<String, AllowedUserApplyRequest> result) {
+            System.out.println("Sent message=[allowed-user-command] with offset=[" + 
+                result.getRecordMetadata().offset() + "]: " +
+                    result.getRecordMetadata().topic());
+          }
           
-        }
+        });
         return new AllowedUserApplyResponse("cdfh3593", "success", "none");
     }
 }
